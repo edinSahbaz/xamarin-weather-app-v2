402ac143e3ddb552a44587e25f5e30f9079b195f
edin.sahbaz <edin.sahbaz@hotmail.com>
2020-08-12 15:22:14 +0200

    Status bar color change

--- WeatherApp.Android/MainActivity.cs
+++ WeatherApp.Android/MainActivity.cs
@@ -1,15 +1,13 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Android.Runtime;
 using Android.Views;
-using Android.Widget;
-using Android.OS;
 
 namespace WeatherApp.Droid
 {
-    [Activity(Label = "Weather App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Weather App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -22,6 +20,12 @@ namespace WeatherApp.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            if (Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.Rgb(16, 16, 58));
+            }
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
