a3499e8ed2ba03eae4f096d3822c2d30368fa18b
edin.sahbaz <edin.sahbaz@hotmail.com>
2020-08-13 16:26:11 +0200

    Added custom font and background

--- WeatherApp/AssemblyInfo.cs
+++ WeatherApp/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("PoppinsBold.ttf", Alias = "FontBold")]
+[assembly: ExportFont("PoppinsMedium.ttf", Alias = "FontMedium")]
+[assembly: ExportFont("PoppinsRegular.ttf", Alias = "FontRegular")]
+[assembly: ExportFont("PoppinsLight.ttf", Alias = "FontLight")]
