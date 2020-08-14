c442579d2e8ab0b5010b68215c634f065aed77b5
edin.sahbaz <edin.sahbaz@hotmail.com>
2020-08-14 13:47:53 +0200

    Geolocator setup

--- WeatherApp/ViewModels/MainPageViewModel.cs
+++ WeatherApp/ViewModels/MainPageViewModel.cs
@@ -1,15 +1,56 @@
-﻿namespace WeatherApp.ViewModels
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace WeatherApp.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
         public MainPageViewModel()
         {
             ApiHelper.InitializeClient();
+
+            LoadCoords = new Command(async () =>
+            {
+                try
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                    var location = await Geolocation.GetLocationAsync(request);
+
+                    if (location != null)
+                    {
+
+                    }
+                }
+                catch (FeatureNotSupportedException fnsEx)
+                {
+                    // Handle not supported on device exception
+                }
+                catch (FeatureNotEnabledException fneEx)
+                {
+                    // Handle not enabled on device exception
+                }
+                catch (PermissionException pEx)
+                {
+                    // Handle permission exception
+                }
+                catch (Exception ex)
+                {
+                    // Unable to get location
+                }
+
+            });
         }
 
         public string Title
         {
             get => "Weather Forecast";
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand LoadCoords { private set; get; }
     }
 }
